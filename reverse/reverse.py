5f88834f90b3674abd34b4f16f463ffdd4156692
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-17 18:18:32 -0400

    second attempt

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -34,30 +34,54 @@ class LinkedList:
             current = current.get_next()
         return False
 
+    def find_node(self, node):
+        # return the node that matches input node 
+        curr = self.head
+        while curr != node:
+            curr = curr.get_next()
+        return curr
+
+
     def reverse_list(self, node, prev):
         # reverse singly linked list, given the node to start
-        # extreme case, None or one item, or input node not in list
-        if self.head is None:
+        # Time: O(n), Space: O(n)
+        # boundary cases, return None
+        # base case: one item, return self
+        # node will be where reverse ocurr 
+        # boundary case: null
+        if node is None:
             return None
-        if self.head.get_next() is None:
-            return None
-        if not self.contains(node.value):
-            return None
-        ## find node in list
-        # current = self.head
-        # while current != node:
-        #     currrent = current.get_next()
-        # simple case, only two items
-        if self.head.get_next() is None:
-            pivot= self.head.get_next()
-            pivot.set_next(self.head)
-            self.head.set_next(None)
-            self.head = pivot
-        # more than two items: recursion 
-        else:
-            current = LinkedList()
-            current.add_to_head(self.head.get_next())
-            current.reverse_list(None, None)
+        # base case, 1 item, not null
+        if node.get_next() is None: 
+            # fix the head
+            self.head = node           
+            return node
+        # reverse (node.next), and returned as rest
+        rest = self.reverse_list(node.get_next(), None)
+        # reverse pointer between node and node.next
+        node.get_next().set_next(node)
+        # fix the tail
+        node.next = None
+        return rest
         
+            
+        
+    # Below is a functional solution using loop, just for my own reference. Time: O(n), Space: O(1)
+    """
+    def reverse_list(self, node, prev): 
+        # iterative method
+        # initialize three pointers
+        # this three pointers will be sliding down the chain until prev sits on the tail (the new head)
 
-
+        prev = None
+        curr = self.head 
+        next = None
+        while curr != None:
+            next = curr.get_next()
+            curr.set_next(prev)
+            prev = curr
+            curr = next
+            
+        self.head = prev        
+        
+    """
