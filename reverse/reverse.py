8ad55d77c15f557c41f0cf62ea6d8ee5dc0fca68
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-17 14:01:00 -0400

    submission

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -20,23 +20,44 @@ class LinkedList:
         node = Node(value)
 
         if self.head is not None:
-            node.set_next(self.head)
-
+            node.set_next(self.head)  
+        # set the node as head    
         self.head = node
 
     def contains(self, value):
         if not self.head:
             return False
-
         current = self.head
-
         while current:
             if current.get_value() == value:
                 return True
-
             current = current.get_next()
-
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # reverse singly linked list, given the node to start
+        # extreme case, None or one item, or input node not in list
+        if self.head is None:
+            return None
+        if self.head.get_next() is None:
+            return None
+        if not self.contains(node.value):
+            return None
+        ## find node in list
+        # current = self.head
+        # while current != node:
+        #     currrent = current.get_next()
+        # simple case, only two items
+        if self.head.get_next() is None:
+            pivot= self.head.get_next()
+            pivot.set_next(self.head)
+            self.head.set_next(None)
+            self.head = pivot
+        # more than two items: recursion 
+        else:
+            current = LinkedList()
+            current.add_to_head(self.head.get_next())
+            current.reverse_list(None, None)
+        
+
+
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
+    """ Ring buffer, not full state"""
     def __init__(self, capacity):
-        pass
-
-    def append(self, item):
-        pass
-
+        self.capacity = capacity
+        self.data = []
+        
+    def append(self, x):
+        # append x in ringbuffer until reaching capacity
+        self.data.append(x)
+        # reach capacity once the ring buffer is full
+        if len(self.data) == self.capacity:
+            # self.cur is the index that will be overwritten
+            # when buffer is full, reset self.cur to 0
+            self.cur = 0
+            # change self class to full:
+            self.__class__ =  self.__Full             
+         
     def get(self):
-        pass
+        return self.data 
+
+    class __Full:
+        """Class implements a full butter"""
+        def append(self, x):
+            #add x, overwrite the oldest (index = self.cur) in the buffer
+            self.data[self.cur] = x
+            # update index with remainder of x/y
+            self.cur = (self.cur + 1 ) % self.capacity
+            #print(f"overwrite index: {self.cur}")
+
+        def get(self):
+            return self.data
+
